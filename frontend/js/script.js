c3be78f492144d21fb65b4f0cbd64454679ac2ef
enzoavalentim <enzo.valentim@estudante.ufla.br>
2025-11-04 14:25:20 -0300

    Ajustes backand lambda

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -19,7 +19,8 @@ const buttonNext = document.querySelector('.btn-next');
 let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const APIResponse = await fetch(`https://u9qdyf01qk.execute-api.us-east-1.amazonaws.com/pokemon?name=${pokemon}`);
+
 
   if (APIResponse.status === 200) {
     const data = await APIResponse.json();
@@ -44,32 +45,38 @@ const renderPokemon = async (pokemon) => {
   const data = await fetchPokemon(pokemon);
 
   if (data) {
-    pokemonImage.style.display = 'block';
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src =
-      data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] ||
-      data['sprites']['front_default'];
+  pokemonImage.style.display = 'block';
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
 
-    pokemonType.innerHTML = data.types.map((t) => t.type.name).join(', ');
-    pokemonHeight.innerHTML = `${data.height / 10} m`;
-    pokemonWeight.innerHTML = `${data.weight / 10} kg`;
+  // usa a imagem normalizada
+  pokemonImage.src = data.sprite || './images/placeholder.png';
 
-    const firstTypeUrl = data.types[0].type.url;
-    const weaknesses = await fetchWeaknesses(firstTypeUrl);
-    pokemonWeaknesses.innerHTML = weaknesses.join(', ') || 'None';
+  // usa os campos simplificados da Lambda
+  pokemonType.innerHTML = data.types.join(', ');
+  pokemonHeight.innerHTML = data.height;
+  pokemonWeight.innerHTML = data.weight;
+  pokemonWeaknesses.innerHTML = (data.weaknesses && data.weaknesses.length)
+  ? data.weaknesses.join(', ')
+  : '-'; // por enquanto, a Lambda não retorna fraquezas
 
-    input.value = '';
-    searchPokemon = data.id;
-  } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not found :c';
-    pokemonNumber.innerHTML = '';
-    pokemonType.innerHTML = '-';
-    pokemonHeight.innerHTML = '-';
-    pokemonWeight.innerHTML = '-';
-    pokemonWeaknesses.innerHTML = '-';
-  }
+  // atualiza o ícone do tipo principal
+  const mainType = data.types[0];
+  typeIcon.src = `./types/${mainType}.png`;
+  typeIcon.alt = mainType;
+
+  input.value = '';
+  searchPokemon = data.id;
+} else {
+  pokemonImage.style.display = 'none';
+  pokemonName.innerHTML = 'Not found :c';
+  pokemonNumber.innerHTML = '';
+  pokemonType.innerHTML = '-';
+  pokemonHeight.innerHTML = '-';
+  pokemonWeight.innerHTML = '-';
+  pokemonWeaknesses.innerHTML = '-';
+}
+
 
   const typeName = data.types[0].type.name;
 
